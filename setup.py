5bbb6aa1ce018cf329855486930b8f8491d2cba8
louisr <l.regnacq@gmail.com>
2021-12-07 15:03:32 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-   name='nrv',
+   name='andi',
    version='0.0.1',
-   description='Neuron Virtualizer',
-   author='Florian Kolbl - Roland Giraud - Louis Regnacq - Thomas Couppey',
-   packages=['nrv'],  #same as name
+   description='Analog Discovery 2 python API',
+   long_description = 'file: README.md',
+   author='Florian Kolbl - Yannick Bornat - Louis Regnacq',
+   packages=['andi'],  #same as name
    include_package_data = True,
-    #data_files = [('nrv', glob('nrv/**/*', recursive=True))], # includes sub-folders - recursive
-
-
-   #install_requires=['bar', 'greek'], #external packages as dependencies
-   #scripts=[
-#        'scripts/cool',
-#            'scripts/skype',
-#           ]
+   url = 'https://github.com/fkolbl/andi',
+   classifiers =[
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent'
+     ],
+    install_requires=['ctypes', 'numpy'], #external packages as dependencies
+    python_requires = '>=3.6'
 )

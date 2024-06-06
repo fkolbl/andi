4524f6b18c3fb5610519758bff6e938d136435dc
Thomas Couppey <thomas.couppey.s@gmail.com>
2024-06-06 16:45:01 +0200

    Bump version: 1.0.1 → 1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="andi-py",
-    version="1.0.1",
+    version="1.1.0",
     description="Analog Discovery 2 python API",
     long_description="file: README.md",
     author="Florian Kolbl - Yannick Bornat - Louis Regnacq",
